6afe24927207d90e23c346dcc231a59e71ff88b5
Alex Perkowski <perkowskialex@gmail.com>
2019-11-26 14:21:18 -0600

    set up mongodb

--- config/database.js
+++ config/database.js
@@ -1,13 +1,14 @@
-var mongoose = require('mongoose');
+let mongoose = require('mongoose');
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
-  useCreateIndex: true
+  useCreateIndex: true,
+  useUnifiedTopology: true
 });
 
 // database connection event
 mongoose.connection.on('connected', function () {
-  console.log(`Mongoose connected to: ${process.env.DATABASE_URL}`);
+  console.log(`Mongoose connected`);
 });
 
 module.exports = mongoose;
