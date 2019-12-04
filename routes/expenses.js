99a043ff6103fd8ab76c11650eb188ba25460a22
Alex Perkowski <perkowskialex@gmail.com>
2019-12-04 09:32:09 -0600

    redefine expenses routes based on REST

--- routes/expenses.js
+++ routes/expenses.js
@@ -2,9 +2,9 @@ let express = require('express')
 let router = express.Router();
 let expensesCtrl = require('../controllers/expenses')
 
-router.get('/expenses/index', expensesCtrl.index)
-router.get('/expenses/:id/new', expensesCtrl.new)
+router.get('/budgets/:id/expenses/', expensesCtrl.index)
+router.get('/budgets/:id/expenses/new', expensesCtrl.new)
 router.get('/expenses/:id', expensesCtrl.show)
-router.post('/budgets/:budgetid', expensesCtrl.create)
+router.post('/budgets/:id/expenses', expensesCtrl.create)
 
 module.exports = router
